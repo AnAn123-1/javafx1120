53269fdec9839e6429d132904b31d4caae23dea3
Hugrage <118659534+Hugrage@users.noreply.github.com>
2022-12-20 11:51:59 +0800

    Add files via upload

---
+++ src/view/view/Dialog.java
@@ -0,0 +1,22 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Dialog extends JDialog {
+    Dialog(String winner){
+        this.setTitle("Congratulations!");
+        this.setVisible(true);
+        this.setLocation(600,400);
+        this.setSize(500,250);
+
+        Container contentpane = this.getContentPane();
+        JLabel jl = new JLabel();
+        ImageIcon img = new ImageIcon("C:\\Users\\Lenovo\\Desktop\\darkchess\\QQ图片20221220113202.jpg");
+        ImageIcon img2 = new ImageIcon("C:\\Users\\Lenovo\\Desktop\\darkchess\\QQ图片20221220113159.jpg");
+        if(winner.equals("BLACK")) jl.setIcon(img);
+        else jl.setIcon(img2);
+        contentpane.add(jl);
+        jl.setHorizontalAlignment(SwingConstants.CENTER);
+    }
+}
